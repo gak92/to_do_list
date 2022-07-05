cec649ecd57f0a1db51213892e434a6307d0acbb
gak92 <44802922+gak92@users.noreply.github.com>
2022-07-05 23:53:33 +0500

    add functionality into index.js to load all tasks from the array

--- src/index.js
+++ src/index.js
@@ -1,4 +1,47 @@
 import _ from 'lodash';
 import './style.css';
 
- 
+const tasksList = [
+  {
+    description: 'Learn Javscript ES6',
+    completed: true,
+    index: 0,
+  },
+  {
+    description: 'Practice CSS3 transition',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Learn Webpack',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'First Capstone project',
+    completed: false,
+    index: 3,
+  },
+  {
+    description: 'Learn arrow functions',
+    completed: false,
+    index: 4,
+  },
+];
+
+const showAllTasks = () => {
+  const todoList = document.querySelector('.todo-list');
+  let tasks = '';
+
+  tasksList.forEach((task) => {
+    tasks += `<li class="todo-item">
+    <input type="checkbox" name="check" id="check">
+    <input type="text" name="task" id="task" value="${task.description}">
+    <i class="fa-solid fa-trash-can"></i>
+    </li>`;
+  });
+
+  todoList.innerHTML = tasks;
+};
+
+showAllTasks();
